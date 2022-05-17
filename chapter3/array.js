51adafc1db5b2d025e2f1146e26d867bd6e447ce
xCall <carlosfaita99@gmail.com>
2022-05-16 21:45:55 -0300

    add

--- chapter3/array.js
+++ chapter3/array.js
@@ -280,3 +280,8 @@ copyArray = [1, 2, 3, 4, 5, 6];
 // copyArray.copyWithin(1, 3, 5);
 console.log('\n copyArray', copyArray.copyWithin(1, 3, 5));
 
+// Convertendo arrays com toString
+console.log('\n tostring', numbers.toString());
+
+// Sepando o array com o join
+console.log('\n join', numbers.join(','));
